4da348924bd7f75a1eae8f76fb2e7ceaed9368be
Your Name <you@example.com>
2022-03-08 16:55:46 +0530

    Fix header logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='header'>
-        <img className='header--image' src="/images/trollFace.png" alt="Troll Face" />
+        <img className='header--image' src={process.env.PUBLIC_URL + "/images/trollFace.png"} alt="Troll Face" />
         <h1 className='header--title'>Meme Generator</h1>
         <p className='header--project'>React Course - Project 3</p>
     </div>
